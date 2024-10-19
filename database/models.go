abbb79b9f1cb5d3bd62bb03b26a33de2db591433
Mario Alberto <mtellez25@alumnos.uaq.mx>
2024-10-19 09:58:47 -0600

    Actualizacion Models

--- database/models.go
+++ database/models.go
@@ -1,13 +1,20 @@
 package database
 
-type Student struct{
-	Student_id int `gorm:"primarykey"`
-	Name string
-	Group string
-	Email string
+type Student struct {
+	Student_id int    `gorm:"primarykey"`
+	Name       string `json:"name"`
+	Group      string `json:"group"`
+	Email      string `json:"email"`
 }
 
-type Subject struct{
-	Subject_id int `gorm:"primarykey"`
-	Name string
+type Subject struct {
+	SubjectID uint   `gorm:"primaryKey"`
+	Name      string `json:"name"`
+}
+
+type Grade struct {
+	GradeID   int     `gorm:"primaryKey"`
+	StudentID int     `json:"student_id"`
+	SubjectID int     `json:"subject_id"`
+	Grade     float64 `json:"grade"`
 }
