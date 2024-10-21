860fee4e93c06f20ccec33a099e6ce30f7c74456
Amari05fc <fercruher@gmail.com>
2024-10-20 20:52:22 -0600

    Get para probar ping

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/Amari05fc/ControlEscolar/database"
+	"github.com/gin-gonic/gin"
 )
+
+type Student struct{
+	Student_id int `json: "student_id"`
+	Name string `json: "name"`
+	Group string `json: "group"`
+	Email string `json: "email"`
+}
+
+type Subject struct{
+	Subject_id int `json: "subject_id"`
+	Name string `json: "name"`
+}
 
 func main() {
 	db, err := database.NewDatabaseDriver()
@@ -17,4 +31,19 @@ func main() {
 	db.AutoMigrate(&database.Student{})
 	db.AutoMigrate(&database.Subject{})
 	fmt.Println("AutoMigrate")
+
+	r := gin.Default()
+	//students := []Student{}
+	//subjects := []Subject{}
+
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/ping", func (c *gin.Context)  {
+		c.JSON(http.StatusOK, gin.H{
+			"message" : "pong",
+		})		
+	})
+
+
+	r.Run(":8001")
+
 }
